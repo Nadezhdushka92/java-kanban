f3989c2075a901da3ab7eac09e006bb496d9fa53
Nadezhda Golubeva <anikinana92@yandex.ru>
2024-05-08 00:31:53 +0300

    Check again Unit test FileBackedTaskManager

--- test/test/FileBackedTaskManagerTest.java
+++ test/test/FileBackedTaskManagerTest.java
@@ -16,21 +16,20 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
-    //private FileBackedTaskManager fileBackedTaskManager;
-    private final File fileSavedHistory = new File("./java-kanban/test/resources/saveTaskManagerTest.csv");
+    private FileBackedTaskManager fileBackedTaskManager;
 
     @BeforeEach
     public void initTests() {
         //String fileSavedHistory = "C:\\Users\\12345\\IdeaProjects\\java-kanban[Sprint7]\\java-kanban\\src\\resources\\saveTaskManager.csv";
         //String fileSavedHistory = Paths.get("src/resources/saveTaskManager.csv").getFileName().toString();
-        //File fileSavedHistory = new File("./java-kanban/test/resources/saveTaskManagerTest.csv");
-        this.manager = new FileBackedTaskManager(fileSavedHistory);
+        File fileSavedHistory = new File("./java-kanban/test/resources/saveTaskManagerTest.csv");
+        fileBackedTaskManager = FileBackedTaskManager.loadFromFile(fileSavedHistory);
+        manager = new FileBackedTaskManager(fileSavedHistory);
     }
 
     @Test
     public void equalsClassTaskIdAndFile() {
         //Prepare
-        FileBackedTaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(fileSavedHistory);
         Task expectedTask = new Task("Задача 1", "После спринта8 сдать спринт9", 10,
                 LocalDateTime.of(2024, 5, 6, 10, 00));
         fileBackedTaskManager.addTask(expectedTask);
@@ -44,7 +43,6 @@ public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskMan
     @Test
     public void equalsClassEpicIdAndFile() {
         //Prepare
-        FileBackedTaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(fileSavedHistory);
         Epic expectedEpic = new Epic("Эпик 1", "Пройти обучение Java", 30,
                 LocalDateTime.of(2024, 5, 6, 10, 30));
         fileBackedTaskManager.addEpic(expectedEpic);
@@ -58,7 +56,6 @@ public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskMan
     @Test
     public void equalsClassSubTaskIdAndFile() {
         //Prepare
-        FileBackedTaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(fileSavedHistory);
         Epic expectedEpic = new Epic("Эпик 1", "Пройти обучение Java", 60,
                 LocalDateTime.of(2024, 5, 6, 11, 30));
         SubTask expectedSubTask = new SubTask("Подзадача 1", "Пройти практику Java", 61,
