36e4f35f303ec1fe81bac111b4010eadd1c1e505
Nadezhda Golubeva <anikinana92@yandex.ru>
2024-05-09 18:16:00 +0300

    Check Run Tests Git Abstract Test Class with related path

--- test/test/FileBackedTaskManagerTest.java
+++ test/test/FileBackedTaskManagerTest.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 //src/java-kanban/test/resources/saveTaskManagerTest.csv
 public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager>  {
-    private final File fileSavedHistory = new File("C:\\Users\\12345\\IdeaProjects\\java-kanban[Sprint7]\\java-kanban\\test\\resources\\saveTaskManagerTest.csv");
+    private final File fileSavedHistory = new File(".\\java-kanban\\test\\resources\\saveTaskManagerTest.csv");
 
     @Override
     public FileBackedTaskManager createTaskManager() {
